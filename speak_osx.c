5ab276316eaf1590f9a4db44c70a27788ff2f0d7
flrl <flrl@users.noreply.github.com>
2015-07-01 16:05:15 +1000

    speech done callback

--- speak_osx.c
+++ speak_osx.c
@@ -1,16 +1,47 @@
 #include <ApplicationServices/ApplicationServices.h>
 
+#include "buffer.h"
 #include "speak.h"
 
 static SpeechChannel g_speech_channel = NULL;
 
+static int speak_next(void) {
+    char *text;
+    size_t len;
+
+    int r = buffer_shift(&text, &len);
+    if (r) return r;
+    if (NULL == text) return 0;
+
+    r = SpeakText(g_speech_channel, text, len);
+
+    free(text);
+    return r;
+}
+
+static void speech_done_cb(SpeechChannel channel, SRefCon refcon) {
+    speak_next();
+}
+
 int speak(const char *text, size_t len) {
+    int r = 0;
+
     if (!g_speech_channel) {
-        int r = NewSpeechChannel(NULL, &g_speech_channel);
+        r = NewSpeechChannel(NULL, &g_speech_channel);
+        if (r) return r;
+
+        r = SetSpeechInfo(g_speech_channel, soSpeechDoneCallBack, speech_done_cb);
         if (r) return r;
     }
 
-    return SpeakText(g_speech_channel, text, len);
+    r = buffer_push(text, len);
+    if (r) return r;
+
+    if (!SpeechBusy()) {
+        r = speak_next();
+    }
+
+    return r;
 }
 
 void shush(void) {
