4b11df243d7ecffedcd01f39ac14c6e890615763
flrl <flrl@users.noreply.github.com>
2015-07-03 17:49:10 +1000

    cleaner interrupt handling

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
+#include <errno.h>
+#include <signal.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
 #include <time.h>
 
 #include "speak.h"
+
+volatile sig_atomic_t g_interrupted = 0;
 
 static void shutdown(int immediately, int code) {
     unload(immediately);
@@ -12,21 +16,53 @@ static void shutdown(int immediately, int code) {
     exit(code);
 }
 
-static void interrupt(int pacifier) {
-    shush();
+static void signal_handler(int signum) {
+    int orig_errno = errno;
+
+    switch (signum) {
+        case SIGINT:
+            g_interrupted++;
+            break;
+
+        default:
+            break;
+    }
+
+    errno = orig_errno;
 }
 
 int main (int argc, const char **argv) {
-    signal(SIGINT, interrupt);
+    struct sigaction action;
+    action.sa_handler = signal_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
 
-    while (1) {
+    sigaction(SIGINT, &action, NULL);
+
+    while (!feof(stdin)) {
         char buf[1024];
         char *p;
 
-        p = fgets(buf, sizeof(buf), stdin);
-        if (NULL == p) break;
+        if (g_interrupted > 1) {
+            shutdown(1, 128 + SIGINT);
+        }
+        else if (g_interrupted == 1) {
+            shush();
+        }
 
-        speak(buf, strlen(buf));
+        p = fgets(buf, sizeof(buf), stdin);
+        if (NULL == p) {
+            if (ferror(stdin)) {
+                if (errno != EINTR) {
+                    perror("fgets");
+                    break;
+                }
+            }
+        }
+        else {
+            g_interrupted = 0;
+            speak(buf, strlen(buf));
+        }
     }
 
     shutdown(0, 0);
